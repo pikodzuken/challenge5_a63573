7220eebf2f946ad24f23a86b7a8a50f15a74e781
Pi <ines.f.ribeiro12@gmail.com>
2026-06-04 18:41:51 +0100

    5.3 passos 1, 2, 3, e 4

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,42 +1,50 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
-    public TextMeshProUGUI scoreText;
-    private int score;
     private float spawnRate = 1.0f;
-
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
+        StartCoroutine(SpawnTarget());
         UpdateScore(0);
     }
-        
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
     // Update is called once per frame
     void Update()
     {
         
     }
-    IEnumerator SpawnTarget()
-        {
-            while (true)
-            {
-                yield return new WaitForSeconds(spawnRate);
-                int index = Random.Range(0, targets.Count);
-                Instantiate(targets[index]);
-
-            }
-        }
-
-        public void UpdateScore(int scoreToAdd)
-        {
-            score += scoreToAdd;
-            scoreText.text = "Score: " + score;
-        }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -3,49 +3,46 @@ using UnityEngine.InputSystem;
 
 public class Target : MonoBehaviour
 {
+
     private Rigidbody targetRb;
-    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
-
-    public ParticleSystem explosionParticle;
+    private GameManager gameManager;
     public int pointValue;
-
+    public ParticleSystem explosionParticle;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
-        targetRb.AddForce(Vector3.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse);
-         targetRb.AddTorque(Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
-            transform.position = new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            Debug.Log("Mouse was clicked");
+            Debug.Log("Mouse Clicked");
             Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
             Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 2.0f);
-
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
             {
-                if (hit.transform == transform)
+                if (hitInfo.transform == transform)
                 {
-                    Destroy(gameObject);
-
-                    Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
-                    gameManager.UpdateScore(pointValue);
+                    if (gameManager.isGameActive)
+                    {
+                        Destroy(gameObject);
+                        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+                        gameManager.UpdateScore(pointValue);
+                    }
                 }
             }
         }
     }
-
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
@@ -55,18 +52,17 @@ public class Target : MonoBehaviour
     {
         return Random.Range(-maxTorque, maxTorque);
     }
-
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("DestroyZone"))
         {
             Destroy(gameObject);
-        }
-    }
-}
+            gameManager.GameOver();
 
+        }   
+    } 
+}
