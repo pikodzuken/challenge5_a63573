4e1b1bade9a62db2f276f6886a429aef5acefc4a
Pi <ines.f.ribeiro12@gmail.com>
2026-06-04 19:19:51 +0100

    5.4 passos 1, 2, e 3

---
+++ Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       button = GetComponent<Button>(); 
+       button.onClick.AddListener(SetDifficulty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
+    }
+}
