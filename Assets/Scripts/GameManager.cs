5a4208b4734ee5c119b26d2adadcd2153a35405f
Pi <ines.f.ribeiro12@gmail.com>
2026-06-04 16:55:25 +0100

    5.2 passos 1, 2, 3, e 4

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
     private float spawnRate = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
         
     // Update is called once per frame
@@ -25,6 +30,14 @@ public class GameManager : MonoBehaviour
                 yield return new WaitForSeconds(spawnRate);
                 int index = Random.Range(0, targets.Count);
                 Instantiate(targets[index]);
+
+                UpdateScore(5);
             }
+        }
+
+        private void UpdateScore(int scoreToAdd)
+        {
+            score += scoreToAdd;
+            scoreText.text = "Score: " + score;
         }
 }
